29b2daf6bd2ba758832681118404c8a7ded9452c
Ruiz Aw <umren@yandex.ru>
2016-04-06 11:06:07 +0300

    getting main sitemap

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const sitemapLocation = "http://www.yves-rocher.ru/sitemap.xml"
 
 func main() {
-	fmt.Println("Hello World!")
+	siteMapXML := getMainSitemap(sitemapLocation)
+	fmt.Println(siteMapXML)
+}
+
+// TODO
+func getSecondarySitemaps() {
+
+}
+
+// getting main sitemap
+func getMainSitemap(xmlURL string) string {
+	resp, err := http.Get(xmlURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp.Body.Close()
+
+	return string(bytes)
 }
