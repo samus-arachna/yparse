a126101a67ba817d6df0bfec9964e13d4c455155
Ruiz Aw <umren@yandex.ru>
2016-04-06 12:14:46 +0300

    getting main sitemap locations

--- main.go
+++ main.go
@@ -2,36 +2,54 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 const sitemapLocation = "http://www.yves-rocher.ru/sitemap.xml"
 
 func main() {
-	siteMapXML := getMainSitemap(sitemapLocation)
-	fmt.Println(siteMapXML)
+	locations := getMainSitemap(sitemapLocation)
+	fmt.Println(locations)
 }
 
 // TODO
-func getSecondarySitemaps() {
-
+func getSecondarySitemaps(xmlData string) {
+	//z := html.NewTokenizer(xmlData)
 }
 
 // getting main sitemap
-func getMainSitemap(xmlURL string) string {
+func getMainSitemap(xmlURL string) []string {
 	resp, err := http.Get(xmlURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	locations := []string{}
+	token := html.NewTokenizer(resp.Body)
+
+	depth := 0
+	for {
+		tt := token.Next()
+		switch tt {
+		case html.ErrorToken:
+			resp.Body.Close()
+			return locations
+		case html.TextToken:
+			if depth > 0 {
+				locations = append(locations, string(token.Text()))
+			}
+		case html.StartTagToken, html.EndTagToken:
+			tn, _ := token.TagName()
+			if string(tn) == "loc" {
+				if tt == html.StartTagToken {
+					depth++
+				} else {
+					depth--
+				}
+			}
+		}
 	}
-
-	resp.Body.Close()
-
-	return string(bytes)
 }
