5b2a9add9c767a4edc17b087a731e2fc42d3dbe9
Ruiz Aw <umren@yandex.ru>
2016-04-11 10:50:10 +0300

    getting pool of products to parse

--- main.go
+++ main.go
@@ -8,8 +8,26 @@ func main() {
 	sitemapLocations := getLocations(sitemapLocation)
 	productLocations := getProductLocations(sitemapLocations)
 
-	product := parseProduct(productLocations[251])
+	//product := parseProduct(productLocations[251])
 
-	// TODO parse first 50 products in a series of 5
-	fmt.Println(product)
+	// TODO parse first 15 products in a series of 3
+	products := productLocations[0:15]
+	runParse(products, 3)
+}
+
+func runParse(products []string, connections int) {
+	// get first slice == number of connections
+	pool := products[0:connections]
+	products = products[connections:]
+
+	fmt.Println(len(pool))
+	fmt.Println(len(products))
+
+	// run connections
+
+	// wait till all complete
+
+	// get another slice, until no more products
+
+	// signalize complete
 }
