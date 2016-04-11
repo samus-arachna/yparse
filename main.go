a5fb2ba10d2a5760327b0bc5f6e0fc6484d7bca2
Ruiz Aw <umren@yandex.ru>
2016-04-11 12:43:43 +0300

    started to work on pool

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 const sitemapLocation = "http://www.yves-rocher.ru/sitemap.xml"
 
@@ -8,24 +11,37 @@ func main() {
 	sitemapLocations := getLocations(sitemapLocation)
 	productLocations := getProductLocations(sitemapLocations)
 
-	//product := parseProduct(productLocations[251])
+	fmt.Println(productLocations[253])
+	product := parseProduct(productLocations[253])
+	fmt.Println(product)
 
-	// TODO parse first 15 products in a series of 3
-	products := productLocations[0:15]
-	runParse(products, 3)
+	// TODO parse first 15 products in a series of 5
+	//products := productLocations[250:260]
+	//runParse(products, 5)
 }
 
 func runParse(products []string, connections int) {
 	// get first slice == number of connections
+	parsed := []map[string]string{}
 	pool := products[0:connections]
 	products = products[connections:]
-
-	fmt.Println(len(pool))
-	fmt.Println(len(products))
+	var wg sync.WaitGroup
 
 	// run connections
+	fmt.Println(len(pool))
+	wg.Add(len(pool))
+	for _, product := range pool {
+		go func(product string) {
+			parsed = append(parsed, parseProduct(product))
+			defer wg.Done()
+		}(product)
+	}
+	wg.Wait()
 
-	// wait till all complete
+	for _, item := range parsed {
+		fmt.Println(item)
+		fmt.Println("")
+	}
 
 	// get another slice, until no more products
 
