17ff1c6a2110c946b7e8de3ec7f27526a49e457d
Ruiz Aw <umren@yandex.ru>
2016-04-15 08:55:20 +0300

    cleanup

--- xml.go
+++ xml.go
@@ -46,30 +46,3 @@ func getXMLProduct(product map[string]string) {
 		fmt.Printf("error: %v\n", err)
 	}
 }
-
-func exampleEncoder() {
-	type Address struct {
-		City, State string
-	}
-	type Person struct {
-		XMLName   xml.Name `xml:"person"`
-		ID        int      `xml:"id,attr"`
-		FirstName string   `xml:"name>first"`
-		LastName  string   `xml:"name>last"`
-		Age       int      `xml:"age"`
-		Height    float32  `xml:"height,omitempty"`
-		Married   bool
-		Address
-		Comment string `xml:",comment"`
-	}
-
-	v := &Person{ID: 13, FirstName: "John", LastName: "Doe", Age: 42}
-	v.Comment = " Need more details. "
-	v.Address = Address{"Hanga Roa", "Easter Island"}
-
-	enc := xml.NewEncoder(os.Stdout)
-	enc.Indent("  ", "    ")
-	if err := enc.Encode(v); err != nil {
-		fmt.Printf("error: %v\n", err)
-	}
-}
